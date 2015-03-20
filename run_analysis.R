3e1143a2b94424dabe48651faff5e257f98ad991
Ellen Yu <ellenyu01@gmail.com>
2015-03-19 23:41:06 -0700

    homework for Course Project Getting and Cleaning data

---
+++ run_analysis.R
@@ -0,0 +1,64 @@
+library(plyr)
+library(dplyr)
+setwd('projects/datascience/R')
+### question 1 - create merged data set
+## question 1 step 1 - load raw datafiles
+x_test<-read.table('UCI HAR Dataset/test/X_test.txt')
+y_test<-read.table('UCI HAR Dataset/test/y_test.txt')
+x_train<-read.table('UCI HAR Dataset/train/X_train.txt')
+y_train<-read.table('UCI HAR Dataset/train/y_train.txt')
+subject_train<-read.table('UCI HAR Dataset/train/subject_train.txt')
+subject_test<-read.table('UCI HAR Dataset/test/subject_test.txt')
+## question 1 step 2 - combine data sets
+test_train<-rbind(x_test,x_train)
+test_train_activity<- rbind(y_test,y_train)
+test_train_subject<-rbind(subject_test,subject_train)
+## question 1 step 3 - rename columns for activity data set as its
+## column names overlap
+test_train_activity<-rename(test_train_activity,activity=V1)
+test_train<-cbind(test_train,test_train_activity)
+test_train_subject<-rename(test_train_subject,subject=V1)
+test_train<-cbind(test_train,test_train_subject)
+### 
+###question 2: Extract only the measurements on the mean and standard dev for
+### measurement
+## read features names
+features<-read.table('UCI HAR Dataset/features.txt')
+## I interpreted the phrase 
+##"only the measurements on the mean and standard deviation for each measurement."
+## to mean to look for any features that contain mean() or std()
+## 
+vv<-c()
+for(i in 1:nrow(features)) {
+  if(grepl('mean()',features[i,2])) {
+    vv<-c(vv,i)
+  }
+  if(grepl('std()',features[i,2])) {
+    vv<-c(vv,i)
+  }
+}
+extract_names<-c(vv,562:563)  
+## 562 and 563 are the two additional fields added (activity and subject)
+## the mean and std columns and activity and subject are put into
+## a new data frame - mean_std_test_train
+mean_std_test_train<-select(test_train,extract_names)
+
+##question 3: Uses descriptive activity names to name the activities in the data set
+activity_labels<-read.table('UCI HAR Dataset/activity_labels.txt')
+activity_labels<-rename(activity_labels,activity=V1, label=V2)
+merged_data<-merge(mean_std_test_train,activity_labels,by.x="activity",by.y="activity",all=TRUE)
+
+##question 4: Appropriately labels the data set with descriptive variable names. 
+for (i in 1:length(names(merged_data))) {
+  if( substr(names(merged_data[i]),1,1)=="V" ) {
+    #find the index number in features -- this would be the column name without "V"
+    feature_index<-as.numeric(substr(names(merged_data)[i],2,nchar(names(merged_data)[i])))
+    names(merged_data)[i] <-as.character(features[feature_index,2]) 
+  }
+  }
+
+##question 5: From the data set in step 4, creates a second, independent tidy data set 
+##with the average of each variable for each activity and each subject
+
+#test_df<-group_by(merged_data,activity,subject)
+tidy_data<-merged_data %>% group_by(activity,subject) %>% summarise_each(funs(mean))
